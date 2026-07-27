11afff74627e3bb7bdd2990c47c0a088ce0ddaab
Octaves VNIT <octaves.vnit@github.com>
2026-07-28 00:45:47 +0530

    Clean up archivesData.js: remove placeholder events

--- data/archivesData.js
+++ data/archivesData.js
@@ -67,39 +67,5 @@ var ARCHIVES_DATA = [
       }
     ]
   },
-  {
-    eventName: "Campus Jam 2025",
-    performances: [
-      {
-        slotName: "Chill Corner",
-        genre: "Lo-Fi / Jazz",
-        youtubeUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ?start=540&rel=0&modestbranding=1"
-      },
-      {
-        slotName: "Pop Hour",
-        genre: "Pop",
-        youtubeUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ?start=640&rel=0&modestbranding=1"
-      },
-      {
-        slotName: "Grand Finale",
-        genre: "Medley",
-        youtubeUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ?start=780&rel=0&modestbranding=1"
-      }
-    ]
-  },
-  {
-    eventName: "Aarohi 2024",
-    performances: [
-      {
-        slotName: "Sunrise Set",
-        genre: "Sufi",
-        youtubeUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ?start=900&rel=0&modestbranding=1"
-      },
-      {
-        slotName: "Night Stage",
-        genre: "EDM Fusion",
-        youtubeUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ?start=1020&rel=0&modestbranding=1"
-      }
-    ]
-  }
+
 ];
