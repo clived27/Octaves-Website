ec8c93b2500455936f0bd9f93ea93347bb489bc6
Dsilva Clive Clarence <bt25cse006@students.vnit.ac.in>
2026-08-11 16:32:59 +0530

    Archives: move Euphoria 2026 to top, default to Legends Never Die on load

--- data/archivesData.js
+++ data/archivesData.js
@@ -3,6 +3,91 @@
 
 var ARCHIVES_DATA = [
   {
+    eventName: "Euphoria 2026",
+    performances: [
+      {
+        slotName: "Legends Never Die",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/viLfwAuGHiM"
+      },
+      {
+        slotName: "Fifi",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/LD4CkZbh_a4"
+      },
+      {
+        slotName: "Kill Dill",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/3tTT48iRwXo"
+      },
+      {
+        slotName: "Kal Chaudhvin ki Raat",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/romRo6NYHAY"
+      },
+      {
+        slotName: "Colours",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/QQhMLejvyxY"
+      },
+      {
+        slotName: "Aaj ki Raat",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/99sjfud7X2U"
+      },
+      {
+        slotName: "Zinda",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/RdrlYppzGh4"
+      },
+      {
+        slotName: "End of Beginning",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/VIXR3Yutyno"
+      },
+      {
+        slotName: "Ye Dil Tum Bin",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/G_lLo-yVGtU"
+      },
+      {
+        slotName: "Old Bollywood Slot",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/y6JkVrByH8c"
+      },
+      {
+        slotName: "New Bollywood Slot",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/YHkt2rscfzc"
+      },
+      {
+        slotName: "Saraswati Vandana",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/DrP_tUponxA"
+      },
+      {
+        slotName: "Locha e Ulfat",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/R5afTZECZM4"
+      },
+      {
+        slotName: "Mohabbat Buri Bimari",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/lbsLrsG0XUc"
+      },
+      {
+        slotName: "Abhi Abhi and Saiyaan",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/-VVXdxbCwOU"
+      },
+      {
+        slotName: "Qawwali",
+        genre: "",
+        youtubeUrl: "https://www.youtube.com/embed/de2dnAKSZFA"
+      }
+    ]
+  },
+  {
     eventName: "Jhankaar IG 2026",
     performances: [
       {
@@ -64,91 +149,6 @@ var ARCHIVES_DATA = [
         slotName: "Soft Bollywood",
         genre: "",
         youtubeUrl: "https://www.youtube.com/embed/ZkIiEW4uJYc"
-      }
-    ]
-  },
-  {
-    eventName: "Euphoria 2026",
-    performances: [
-      {
-        slotName: "Saraswati Vandana",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/DrP_tUponxA"
-      },
-      {
-        slotName: "Fifi",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/LD4CkZbh_a4"
-      },
-      {
-        slotName: "Kill Dill",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/3tTT48iRwXo"
-      },
-      {
-        slotName: "Kal Chaudhvin ki Raat",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/romRo6NYHAY"
-      },
-      {
-        slotName: "Colours",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/QQhMLejvyxY"
-      },
-      {
-        slotName: "Aaj ki Raat",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/99sjfud7X2U"
-      },
-      {
-        slotName: "Zinda",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/RdrlYppzGh4"
-      },
-      {
-        slotName: "End of Beginning",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/VIXR3Yutyno"
-      },
-      {
-        slotName: "Ye Dil Tum Bin",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/G_lLo-yVGtU"
-      },
-      {
-        slotName: "Old Bollywood Slot",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/y6JkVrByH8c"
-      },
-      {
-        slotName: "New Bollywood Slot",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/YHkt2rscfzc"
-      },
-      {
-        slotName: "Legends Never Die",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/viLfwAuGHiM"
-      },
-      {
-        slotName: "Locha e Ulfat",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/R5afTZECZM4"
-      },
-      {
-        slotName: "Mohabbat Buri Bimari",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/lbsLrsG0XUc"
-      },
-      {
-        slotName: "Abhi Abhi and Saiyaan",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/-VVXdxbCwOU"
-      },
-      {
-        slotName: "Qawwali",
-        genre: "",
-        youtubeUrl: "https://www.youtube.com/embed/de2dnAKSZFA"
       }
     ]
   }
